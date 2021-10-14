cdd9a1543944a337e8847ff97309c8c7447a07bf
choiyoungkwon12 <hpwh4457@naver.com>
2021-10-14 15:36:10 +0900

    ìchap05

---
+++ src/main/java/chap05/config/AppCtx.java
@@ -0,0 +1,34 @@
+package chap05.config;
+
+import chap05.spring.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = {"chap05"},
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {NoProduct.class, ManualBean.class}))
+public class AppCtx {
+
+    @Bean
+    @Qualifier("printer")
+    public MemberPrinter memberPrinter1() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    @Qualifier("summaryPrinter")
+    public MemberSummaryPrinter memberPrinter2() {
+        return new MemberSummaryPrinter();
+    }
+
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
+}
